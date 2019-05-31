e985a392d2f5f551edb0cae2c30d333ffc2bddc4
Fernando Jiménez <the.merck@gmail.com>
2019-05-31 14:50:25 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,183 @@
+import { SixphereLitElement, html, css } from '/web_modules/@sixphere/lit-element.js'
+import { render } from '/web_modules/lit-html.js'
+
+import { mainStyle } from './styles.js'
+
+/**
+ * Allowed statuses. Based on Bootstrap colors
+ * @see https://getbootstrap.com/docs/4.3/utilities/colors/
+ */
+const STATUSES = {
+    SUCCESS: "success",
+    WARNING: "warning",
+    DANGER: "danger"
+}
+
+/**
+ * Custom Element for showing numeric data through an injected service.
+ * 
+ */
+class PolarisNumericDisplay extends SixphereLitElement {
+
+    /* LIFECYCLE METHODS */
+
+    constructor() {
+        super()
+
+        this.title = ""
+        this._status = ""
+        this._value = null
+        this.min = -Infinity
+        this.max = Infinity
+
+        this.updateComplete.then(this.onUpdateComplete.bind(this))
+    }
+
+    /* STATICS METHODS */
+
+    static get is() { return "polaris-numeric-display" }
+
+    static get properties() {
+        return {
+            /**
+             * Title to be displayed by the component
+             */
+            title: { type: String }, 
+            /**
+             * Statuses based on Bootstrap colors
+             * @see https://getbootstrap.com/docs/4.3/utilities/colors/
+             */
+            _status: { type: String , reflect: true, attribute: 'status'}, 
+            /**
+             * Low data boundary
+             */
+            min: { type: Number},
+            /**
+             * High data boundary
+             */
+            max: { type: Number},
+            /**
+             * Fixed value
+             */
+            value: { type: Number},
+        }
+    }
+
+    /**
+     * Services injection method using by Sixphere Dependency Injection Wrapper library.
+     * 
+     * @see https://www.npmjs.com/package/@sixphere/diwrapper
+     * 
+     * @param {Service} service Data service. Must implement a getData async method.
+     */
+    __inject(service) {
+        this.service = service
+    }
+
+    /* METHODS */
+
+    /**
+     * UpdateComplete event handler.
+     */
+    async onUpdateComplete() {
+        if (!this.hasService && !this.value)
+            throw new Error(`You must define an attribute value or inject a service for the component [ ${PolarisNumericDisplay.name} ] before init`)
+
+        if (this.hasService)
+            await this.getData()
+
+        this.status = this.calculateStatus(this.value)
+        render(this.renderCircle(), this.$refs.wrapper)
+    }
+
+    /**
+     * Getting data through the injected service and redering it as HTML
+     */
+    async getData() {
+        try {
+            let response = await this.service.getData()
+            this.value = response.data
+        } catch (error) {
+            this.data = this.noDataSymbol;
+        }
+    }
+
+    calculateStatus(data) {
+        if(this.status)
+            return this.status
+
+        if(this.min <= data && data <= this.max)
+            return STATUSES.SUCCESS
+
+        return STATUSES.DANGER
+    }
+
+
+    /* Computed properties */
+
+    get noDataSymbol(){
+        return "·"
+    }
+
+    get hasService() {
+        return typeof this.service === "object" && typeof this.service.getData === "function"
+    }
+
+    get status() {
+        return this._status
+    }
+
+    set status(status) {
+        this._status = status
+    }
+
+    get value() {
+        return this._value
+    }
+
+    set value(value) {
+        this._value = value
+    }
+
+
+    /* RENDERING */
+
+    static get styles() {
+        return [ 
+            mainStyle
+        ]
+    }
+
+    renterTitle() {
+        return html`
+            <h4 class="poalris-numeric-display__title ${this.statusClass()}" ref="title">${this.title}</div>
+        `
+    }
+
+    statusClass() {
+        return Object.values(STATUSES).includes(this.status) ? `status-${this.status}` : "";
+    }
+
+    renderCircle() {
+        let title = this.title ? this.renterTitle() : "" ;
+        let status = this.statusClass()
+
+        return html`
+            <div ref="circle" class="poalris-numeric-display__circle ${status}">
+                <div ref="display" class="poalris-numeric-display__display ${status}">${this.value || this.noDataSymbol}</div>
+            </div>
+            ${title}
+        `
+    }
+
+    render() {
+        let circle = this.renderCircle()
+        return html`
+            <div ref="wrapper" class="polaris-numeric-display__wrapper">
+                ${circle}
+            </div>
+        `
+    }
+}
+
+export { PolarisNumericDisplay }
---
+++ src/styles.js
@@ -0,0 +1,91 @@
+import { css } from '/web_modules/@sixphere/lit-element.js'
+
+let mainStyle = css`
+:host {
+    --blue: #2196f3;
+    --indigo: #3f51b5;
+    --purple: #9c27b0;
+    --pink: #e91e63;
+    --red: #f44336;
+    --orange: #ff9800;
+    --yellow: #ffeb3b;
+    --green: #4caf50;
+    --teal: #009688;
+    --cyan: #00bcd4;
+    --white: #fff;
+    --gray: #6c757d;
+    --gray-dark: #343a40;
+    --primary: #009688;
+    --secondary: #6c757d;
+    --success: #4caf50;
+    --info: #03a9f4;
+    --warning: #ff5722;
+    --danger: #f44336;
+    --light: #f5f5f5;
+    --dark: #424242;
+    --breakpoint-xs: 0;
+    --breakpoint-sm: 576px;
+    --breakpoint-md: 768px;
+    --breakpoint-lg: 992px;
+    --breakpoint-xl: 1200px;
+    --font-family-sans-serif: "Roboto", "Helvetica", "Arial", sans-serif;
+    --font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace; 
+}
+
+:host {
+    font-family: var(--font-family-sans-serif);
+    font-weight: 300;
+}
+
+h4 {
+    font-size: 20px;
+    font-weight: 300;
+    margin: 0;
+}
+
+.polaris-numeric-display__wrapper {
+    padding: 10px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.poalris-numeric-display__circle{
+    border: 1px solid #dedede;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.poalris-numeric-display__display{
+    font-size: 55px;
+}
+
+.poalris-numeric-display__title {
+    margin-top: 0.5em;
+}
+
+.status-success {
+    color: var(--success);
+    border-color: var(--success);
+}
+
+.status-warning {
+    color: var(--warning);
+    border-color: var(--warning);
+}
+
+.status-danger {
+    color: var(--danger);
+    border-color: var(--danger);
+}
+`
+
+export { mainStyle }
